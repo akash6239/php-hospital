5dd6f72725c63c2cd86866e97e38cc08b32bb6e7
akash6239 <akashcoc98155@gmail.com>
2025-04-02 22:14:13 +0530

    change connection

--- connection.php
+++ connection.php
@@ -1,15 +1,4 @@
 <?php
-// $username = "root";
-// $password = "";
-// $server = "localhost";
-// $database = "hospital";
-
-// $response = mysqli_connect($server,$username,$password,$database);
-
-// if (!$response){
-//     echo "Sorry Brother";
-// }
-
 
 $response = mysqli_connect("localhost","root","","hospital") or die("connection failed");
 
